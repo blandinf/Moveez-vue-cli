2994f321cdeaa8329ca3b4c98216dc3e3f7cdb12
Florian <florianblandin-iut@outlook.fr>
2019-06-06 22:04:17 +0200

    add many libraries

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import Toasted from 'vue-toasted';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/main.scss'
+import Router from "vue-router";
 
-Vue.config.productionTip = false
+library.add(faSpinner);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.use(BootstrapVue);
+Vue.use(Toasted);
+Vue.use(Router);
+
+const routes = [
+  {
+    name: 'movie.show',
+    path: '/movie/:movie',
+    component: () => import('./components/movies/Show.vue'),
+    props: true,
+  },
+];
+
+const router = new Router({
+  routes
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
+  router: router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
